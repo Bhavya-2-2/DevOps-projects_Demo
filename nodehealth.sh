10a4dc1191018b69410e71f296ab64561f1ed209
Bhavya <kpbhavyabhavya95253@gmail.com>
2024-10-31 12:34:46 +0530

    Add nodehealth.sh script

---
+++ nodehealth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# wweneed to provide autore and script details]
+#############################
+#Author : Bhavya (meta deta)
+# Date:25/10/24
+#
+#this script outputs the node health
+#
+#
+#version : v1
+#############################
+set -x
+set -e
+set -o pipefail
+create a user,creat a file, and add the user name to file
+df - h
+free - g
+nproc
